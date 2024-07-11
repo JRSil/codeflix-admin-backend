4a18b04dabbd6ed60d35bf763e48d2b3007ffba6
Edilton Junior <edilton.bezerra@marketup.com>
2024-07-11 18:09:44 +0000

    feat: create database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CategoriesModule } from './categories/categories.module';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { CategoryModel } from '@core/category/infra/db/sequelize/category.model';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [CategoriesModule],
+  imports: [
+    DatabaseModule,
+    CategoriesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
