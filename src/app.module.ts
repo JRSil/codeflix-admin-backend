ccb5cae3a3077bd7ed8fca8ea07ace33370d00a5
Edilton Junior <edilton.bezerra@marketup.com>
2024-07-15 13:50:41 -0300

    feat: organize nest modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { CategoriesModule } from './categories/categories.module';
-import { SequelizeModule } from '@nestjs/sequelize';
-import { CategoryModel } from '@core/category/infra/db/sequelize/category.model';
-import { DatabaseModule } from './database/database.module';
+import { CategoriesModule } from './nest-modules/categories-module/categories.module';
+import { DatabaseModule } from './nest-modules/database-module/database.module';
+import { ConfigModule } from './nest-modules/config-module/config.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     DatabaseModule,
     CategoriesModule,
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+  ]
 })
 export class AppModule { }
